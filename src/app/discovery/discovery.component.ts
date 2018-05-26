409e7bb8772ef999d50ae6c856f07e314249ba08
Paweł Kadłubowski <pkadlubowski@magkom.pl>
2018-05-26 22:07:44 +0200

    discovery - wyswietlanie tabow ngif

--- src/app/discovery/discovery.component.ts
+++ src/app/discovery/discovery.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-discovery',
-  templateUrl: './discovery.component.html',
-  styleUrls: ['./discovery.component.css']
+    selector: 'app-discovery',
+    templateUrl: './discovery.component.html',
+    styleUrls: ['./discovery.component.css']
 })
 export class DiscoveryComponent implements OnInit {
+    public selected = 'probe'; // probe | job | mapping | scheduler | task
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
